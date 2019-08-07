89e9762bb7b4817b647231c88cd60a84293b83d6
Hieu <lordiga@gmail.com>
2019-08-07 09:47:50 -0400

    Save file on SDCard Add Compass on screen

--- app/src/main/java/com/example/hieubui/carcam/MainCam.java
+++ app/src/main/java/com/example/hieubui/carcam/MainCam.java
@@ -702,14 +702,14 @@ public class MainCam extends Activity implements IBaseGpsListener {
         // To be safe, you should check that the SDCard is mounted
         // using Environment.getExternalStorageState() before doing this.
 
-        File mediaStorageDir = new File("/storage/sdcard0/CarCam");
+        File mediaStorageDir = new File("/storage/sdcard0/carcam");
         // This location works best if you want the created images to be shared
         // between applications and persist after your app has been uninstalled.
 
         // Create the storage directory if it does not exist
         if (! mediaStorageDir.exists()){
             if (! mediaStorageDir.mkdirs()){
-                Log.d("CarCam", "failed to create directory");
+                Log.d("carcam", "failed to create directory");
                 return null;
             }
         }
