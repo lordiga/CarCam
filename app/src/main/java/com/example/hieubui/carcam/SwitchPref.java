ab2f553464f1cb48b3a927c14a1d4270d8e73435
Hieu Bui <lordiga@gmail.com>
2020-01-08 00:12:20 -0500

    Use customized SwitchPref to fix the auto reset switch on preference

---
+++ app/src/main/java/com/example/hieubui/carcam/SwitchPref.java
@@ -0,0 +1,19 @@
+package com.example.hieubui.carcam;
+
+import android.content.Context;
+import android.preference.SwitchPreference;
+import android.util.AttributeSet;
+
+public class SwitchPref extends SwitchPreference {
+    public SwitchPref(Context context) {
+        super(context);
+    }
+
+    public SwitchPref(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public SwitchPref(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+    }
+}
